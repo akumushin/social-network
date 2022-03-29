51b83399be739f83d8b4363709abb68b250d1d45
thuong <thuong@THUONG_01>
2022-03-30 00:51:11 +0900

    initial

--- social-network-normal/src/main/java/com/socialnetwork/normal/SocialNetworkNormalApplication.java
+++ social-network-normal/src/main/java/com/socialnetwork/normal/SocialNetworkNormalApplication.java
@@ -1,4 +1,4 @@
-package com.socialnetwork;
+package com.socialnetwork.normal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
