0a5d2473e5e4ed5d84327e04327cbcc4d564c572
thuong <thuong@THUONG_01>
2022-03-30 00:32:32 +0900

    instance

--- social-network-common/src/main/java/com/socialnetwork/SocialNetworkRepositoryApplication.java
+++ social-network-common/src/main/java/com/socialnetwork/SocialNetworkRepositoryApplication.java
@@ -1,4 +1,4 @@
-package com.socialnetwork.common;
+package com.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
