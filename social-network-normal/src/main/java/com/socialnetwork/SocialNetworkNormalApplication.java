bad84821fe0e13288059f64f360f8f6723d11143
thuong <thuong@THUONG_01>
2022-03-30 00:47:46 +0900

    initial

---
+++ social-network-normal/src/main/java/com/socialnetwork/SocialNetworkNormalApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkNormalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkNormalApplication.class, args);
+	}
+
+}
