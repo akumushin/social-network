103538c17edcdf6f69cadf366e14b00908a0b069
thuong <thuong@THUONG_01>
2022-03-30 00:27:17 +0900

    a

---
+++ social-network-admin/src/main/java/com/socialnetwork/SocialNetworkAdminApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkAdminApplication.class, args);
+	}
+
+}
