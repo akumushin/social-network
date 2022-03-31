9473238ed09b35bbe941ec38ab534c66536d6f54
thuong <thuong@THUONG_01>
2022-04-01 00:52:53 +0900

    ss

--- social-network-common/src/main/java/com/socialnetwork/common/entities/page/PageInfo.java
+++ social-network-common/src/main/java/com/socialnetwork/common/entities/page/PageInfo.java
@@ -1,7 +1,5 @@
 package com.socialnetwork.common.entities.page;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
@@ -11,12 +9,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.socialnetwork.common.entities.blockcause.BlockCauseInfo;
+import com.socialnetwork.common.entities.BaseSocialEntity;
 import com.socialnetwork.common.entities.user.UserInfo;
-import com.socialnetwork.common.types.BooleanConvert;
 import com.socialnetwork.common.types.PageType;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 /**
  * 
  * @author thuong
@@ -25,53 +23,33 @@ import lombok.Data;
 @Entity
 @Table(name="m_page_info")
 @Data
-public class PageInfo {
+@EqualsAndHashCode(callSuper = false)
+public class PageInfo extends BaseSocialEntity{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	//page_id
 	@Id
 	@Column(name="page_id")
 	private Long pageId;
+	
 	//owner_id
 	@Column(name="owner_id", nullable = false)
 	private Long ownerId;
+	
 	//page_name
 	@Column(name="page_name", length = 200, nullable = false)
 	private String pageName;
+	
 	//page_type
 	@Column(name="page_type", nullable = false)
 	@Convert(converter = PageType.Convert.class)
 	private PageType pageType;
-	//create_at
-	@Column(name="create_at")
-	private LocalDateTime createAt;
-	//update_at
-	@Column(name="update_at")
-	private LocalDateTime updateAt;
-	//del_flag
-	@Column(name="del_flag", nullable = false)
-	@Convert(converter = BooleanConvert.class)
-	private boolean delFlag;
-	//del_at
-	@Column(name="del_at")
-	private LocalDateTime delAt;
-	//block_flag
-	@Column(name="block_flag", nullable = false)
-	@Convert(converter = BooleanConvert.class)
-	private boolean blockFlag;
-	//block_at
-	@Column(name="block_at")
-	private LocalDateTime blockAt;
-	//block_cause_id
-	@Column(name="block_cause_id")
-	private Integer blockCauseId;
 
 	//relationship
-	
 	
 	@JoinColumn(name="owner_id", updatable = false, insertable = false)
 	@ManyToOne(fetch = FetchType.LAZY)
 	private UserInfo ownerInfo;
-	
-	@JoinColumn(name="block_cause_id", updatable = false, insertable = false)
-	@ManyToOne(fetch = FetchType.LAZY)
-	private BlockCauseInfo blockCauseInfo;
 }
